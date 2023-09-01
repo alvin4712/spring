4d46977d2f4fc9098d9f9c641b02487209a7f374
Alvaro Santos <alvarosantos2803@gmail.com>
2023-09-01 20:24:26 +0000

    initial commit

---
+++ src/main/java/br/com/arcs/springstudies/SpringStudiesApplication.java
@@ -0,0 +1,13 @@
+package br.com.arcs.springstudies;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringStudiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/arcs/springstudies/SpringStudiesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.arcs.springstudies;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
